556821d0dd838f1933ce2f5bc061b89ca23dbdf2
phantomboy0 <talebiamirhosain@gmail.com>
2024-03-13 22:26:59 +0330

    changeed server

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 //const fs = require("fs");
-const fs = require("@cyclic.sh/s3fs")(
-  "cyclic-vast-plum-ladybug-slip-us-west-1"
-);
+const fs = require("@cyclic.sh/s3fs")("cyclic-clear-yak-pocketbook-eu-west-1");
 const cors = require("cors");
 const cron = require("node-cron");
 const { log } = require("console");
